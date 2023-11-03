d79613a045f672abdf167f9535a6175f5d22cb34
g-aditi <aditi.ganapathi.2000@gmail.com>
2023-11-03 15:20:53 -0700

    Removed owlready, simplified and expanded ontology into list format

---
+++ properties.py
@@ -0,0 +1,134 @@
+colors = [
+    'purple',
+    'pink',
+    'neon pink',
+    'pastel pink',
+    'dark blue',
+    'light blue',
+    'neon blue',
+    'pastel blue',
+    'teal',
+    'dark green',
+    'light green',
+    'neon green',
+    'pastel green',
+    'yellow',
+    'amber',
+    'orange',
+    'red',
+    'maroon',
+    'brown',
+    'black',
+    'gray',
+    'white',
+    'gold',
+    'silver',
+    'beige'
+]
+
+shape = [
+    'square',
+    'rectangular',
+    'circular',
+    'triangular',
+    'round',
+    'blocky',
+    'pointy',
+    'pear_shaped',
+    'conical',
+    'cone',
+    'cylindrical',
+    'spherical',
+    'solid',
+    'liquid',
+    'oblate'
+]
+
+size = [
+    'big',
+    'broad',
+    'bulky',
+    'fat',
+    'giant',
+    'heavy',
+    'huge',
+    'lanky',
+    'large',
+    'little',
+    'narrow',
+    'plump',
+    'short',
+    'slim',
+    'small',
+    'stout',
+    'tall',
+    'thick',
+    'thin',
+    'tiny',
+    'wide'
+]
+
+food_properties = [
+    'rotten',
+    'fresh'
+    'ripe',
+    'unripe'
+]
+
+emotion = [
+    'bold',
+    'brave',
+    'cheerful',
+    'comfortable',
+    'excited',
+    'festive',
+    'optimistic',
+    'proud',
+    'wonderful',
+    'happy',
+    'joyful',
+    'serene',
+    'upbeat',
+    'dirty',
+    'dejected',
+    'irritated',
+    'pessimistic',
+    'tearful',
+    'tense',
+    'tired',
+    'weak',
+    'angry',
+    'gloomy',
+    'grumpy',
+    'mad',
+    'mad',
+    'sad',
+    'guilty',
+    'anxious',
+    'cautious',
+    'shy',
+    'calm',
+    'confident',
+    'surprised'
+]
+
+numbers = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+]
+
+quantity = [
+    'a little',
+    'a lot',
+    'more',
+    'less',
+    'some'
+]
