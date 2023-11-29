d5de56b6b609b7b3d48000ce624197254fd5e950
g-aditi <aditi.ganapathi.2000@gmail.com>
2023-11-28 18:53:33 -0700

    updated random prompts

--- level3_rules.py
+++ level3_rules.py
@@ -1,4 +1,5 @@
 import grammar
+import random
 
 # association rules using description logics
 
@@ -12,19 +13,21 @@ head2 = []
 
 with open("level3.txt", "w") as level3_file, open("level1.txt", "r") as level1_file:
     level1_file_lines = level1_file.readlines()
-    for i in range(19):
+    for i in range(50):
         if i % 2 == 0:
-            head1.append(level1_file_lines[i][:-1])
+            head1.append(random.choice(level1_file_lines)[:-1])
+        # head1[i] = head1[i][:-1]  
 
-    for j in range(80, 99):
+    for j in range(60, 99):
         if j % 2 == 0:
-            head2.append(level1_file_lines[j][:-1])
+            head2.append(random.choice(level1_file_lines)[:-1])
+        # head2[j] = head2[j][:-1] 
 
     def dl_level3():
-        for phrase1 in head1:
-            for preposition in grammar.prepositions:
-                for phrase2 in head2:
-                    level3_file.write(phrase1 + " " + preposition + " " + phrase2)
-                    level3_file.write("\n")
+        for i in range(600):
+            level3_file.write((random.choice(head1) + " " + 
+                          random.choice(grammar.prepositions) + " " + 
+                          random.choice(head2)))
+            level3_file.write("\n")
 
     dl_level3()
